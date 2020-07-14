6febd98898fbed5f92e7b160664777f8454e40c1
stal111 <stal111@gmx.de>
2020-07-14 12:12:22 +0200

    Improve some old PillarBlock code

--- src/main/java/com/stal111/forbidden_arcanus/block/PillarBlock.java
+++ src/main/java/com/stal111/forbidden_arcanus/block/PillarBlock.java
@@ -4,13 +4,15 @@ import com.stal111.forbidden_arcanus.block.properties.ConnectedBlockType;
 import com.stal111.forbidden_arcanus.util.VoxelShapeHelper;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.IWaterLoggable;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.pathfinding.PathType;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.EnumProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.tags.FluidTags;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
@@ -19,50 +21,51 @@ import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 
-public class PillarBlock extends WaterloggedBlock {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PillarBlock extends Block implements IWaterLoggable {
 
 	public static final EnumProperty<ConnectedBlockType> TYPE = EnumProperty.create("type", ConnectedBlockType.class);
 	public static final EnumProperty<Direction.Axis> AXIS = BlockStateProperties.AXIS;
+	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
-	private static final VoxelShape[]
-			SHAPE = {
+	private static final VoxelShape[] SHAPE = {
 			Block.makeCuboidShape(0, 14, 0, 16, 16, 16),
 			Block.makeCuboidShape(1, 13, 1, 15, 14, 15),
 			Block.makeCuboidShape(2, 0, 2, 14, 16, 14),
 			Block.makeCuboidShape(0, 0, 0, 16, 2, 16),
-			Block.makeCuboidShape(1, 2, 1, 15, 3, 15) };
+			Block.makeCuboidShape(1, 2, 1, 15, 3, 15)
+	};
 
-	private VoxelShape generateShape(BlockState state) {
-		if (state.get(TYPE) == ConnectedBlockType.SINGLE) {
-			return VoxelShapeHelper.rotateShapeAxis(VoxelShapeHelper.combineAll(SHAPE), state.get(AXIS));
-		} else if (state.get(TYPE) == ConnectedBlockType.TOP) {
-			return VoxelShapeHelper.rotateShapeAxis(VoxelShapeHelper.combineAll(SHAPE[0], SHAPE[1], SHAPE[2]), state.get(AXIS));
-		} else if (state.get(TYPE) == ConnectedBlockType.CENTER) {
-			return VoxelShapeHelper.rotateShapeAxis(SHAPE[2], state.get(AXIS));
-		} else {
-			return VoxelShapeHelper.rotateShapeAxis(VoxelShapeHelper.combineAll(SHAPE[2], SHAPE[3], SHAPE[4]), state.get(AXIS));
+	private VoxelShape getShapeForState(BlockState state) {
+		List<VoxelShape> list = new ArrayList<>();
+		list.add(SHAPE[2]);
+
+		switch (state.get(TYPE)) {
+			case TOP: list.addAll(Arrays.asList(SHAPE[0], SHAPE[1])); break;
+			case BOTTOM: list.addAll(Arrays.asList(SHAPE[3], SHAPE[4])); break;
+			case SINGLE: list = Arrays.asList(SHAPE); break;
 		}
+
+		return VoxelShapeHelper.rotateShapeAxis(VoxelShapeHelper.combineAll(list), state.get(AXIS));
 	}
 
 	public PillarBlock(Properties properties) {
 		super(properties);
 		this.setDefaultState(this.stateContainer.getBaseState().with(AXIS, Direction.Axis.Y).with(TYPE, ConnectedBlockType.SINGLE).with(WATERLOGGED, false));
 	}
-
-	@Override
-	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-		builder.add(TYPE, AXIS, WATERLOGGED);
-	}
 	
 	@Override
 	public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context) {
-		return this.generateShape(state);
+		return this.getShapeForState(state);
 	}
 
 	@Override
 	public BlockState getStateForPlacement(BlockItemUseContext context) {
 		FluidState fluidstate = context.getWorld().getFluidState(context.getPos());
-		boolean flag = fluidstate.isTagged(FluidTags.WATER) && fluidstate.getLevel() == 8;
+		boolean flag = fluidstate.getFluid() == Fluids.WATER;
 		return super.getStateForPlacement(context).with(TYPE, ConnectedBlockType.SINGLE).with(AXIS, context.getFace().getAxis()).with(WATERLOGGED, flag);
 	}
 
@@ -71,44 +74,19 @@ public class PillarBlock extends WaterloggedBlock {
 		if (state.get(WATERLOGGED)) {
 			world.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(world));
 		}
-		return state.with(TYPE, this.tryConnect(state, world, currentPos));
+		return super.updatePostPlacement(state, facing, facingState, world, currentPos, facingPos).with(TYPE, tryConnect(state, world, currentPos));
 	}
 
 	public ConnectedBlockType tryConnect(BlockState state, IWorld world, BlockPos pos) {
 		Direction.Axis axis = state.get(AXIS);
-		BlockState stateDown;
-		BlockState stateUp;
 
-		if (axis == Direction.Axis.X) {
-			stateDown = world.getBlockState(pos.west());
-			stateUp = world.getBlockState(pos.east());
-		} else if (axis == Direction.Axis.Z) {
-			stateDown = world.getBlockState(pos.south());
-			stateUp = world.getBlockState(pos.north());
-		} else {
-			stateDown = world.getBlockState(pos.down());
-			stateUp = world.getBlockState(pos.up());
-		}
+		BlockState stateDown = world.getBlockState(pos.offset(Direction.getFacingFromAxis(Direction.AxisDirection.NEGATIVE, axis)));
+		BlockState stateUp = world.getBlockState(pos.offset(Direction.getFacingFromAxis(Direction.AxisDirection.POSITIVE, axis)));
 
-		boolean axisUpEqual = false;
-		boolean axisDownEqual = false;
+		boolean axisUpEqual = stateUp.getBlock() instanceof PillarBlock && stateUp.get(AXIS) == axis;
+		boolean axisDownEqual = stateDown.getBlock() instanceof PillarBlock && stateDown.get(AXIS) == axis;
 
-		if (stateUp.getBlock() instanceof PillarBlock) {
-			axisUpEqual = stateUp.get(AXIS) == axis;
-		}
-		if (stateDown.getBlock() instanceof PillarBlock) {
-			axisDownEqual = stateDown.get(AXIS) == axis;
-		}
-
-		if (axisUpEqual && axisDownEqual) {
-			return ConnectedBlockType.CENTER;
-		} else if (axisUpEqual) {
-			return ConnectedBlockType.BOTTOM;
-		} else if (axisDownEqual) {
-			return ConnectedBlockType.TOP;
-		} else {
-			return ConnectedBlockType.SINGLE;
-		}
+		return ConnectedBlockType.getTypeForConnections(axisUpEqual, axisDownEqual, axis);
 	}
 
 	@Override
@@ -127,5 +105,20 @@ public class PillarBlock extends WaterloggedBlock {
 			default:
 				return state;
 		}
+	}
+
+	@Override
+	public boolean allowsMovement(BlockState state, IBlockReader worldIn, BlockPos pos, PathType type) {
+		return false;
+	}
+
+	@Override
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		builder.add(TYPE, AXIS, WATERLOGGED);
+	}
+
+	@Override
+	public FluidState getFluidState(BlockState state) {
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
 	}
 }
