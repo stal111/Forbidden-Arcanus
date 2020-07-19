f3af9b37efa946656e98f855acdd92492b075a27
stal111 <stal111@gmx.de>
2020-07-19 21:36:23 +0200

    Fix Candelabra Block Tags

--- src/main/java/com/stal111/forbidden_arcanus/util/ModTags.java
+++ src/main/java/com/stal111/forbidden_arcanus/util/ModTags.java
@@ -1,5 +1,6 @@
 package com.stal111.forbidden_arcanus.util;
 
+import com.stal111.forbidden_arcanus.Main;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.tags.BlockTags;
@@ -13,9 +14,9 @@ public class ModTags {
     }
 
     public static class Blocks {
-        public static final ITag.INamedTag<Block> CANDELABRAS = BlockTags.makeWrapperTag("candelabras");
-        public static final ITag.INamedTag<Block> STANDING_CANDELABRAS = BlockTags.makeWrapperTag("standing_candelabras");
-        public static final ITag.INamedTag<Block> WALL_CANDELABRAS = BlockTags.makeWrapperTag("wall_candelabras");
-        public static final ITag.INamedTag<Block> HANGING_CANDELABRAS = BlockTags.makeWrapperTag("hanging_candelabras");
+        public static final ITag.INamedTag<Block> CANDELABRAS = BlockTags.makeWrapperTag(Main.MOD_ID + ":candelabras");
+        public static final ITag.INamedTag<Block> STANDING_CANDELABRAS = BlockTags.makeWrapperTag(Main.MOD_ID + ":standing_candelabras");
+        public static final ITag.INamedTag<Block> WALL_CANDELABRAS = BlockTags.makeWrapperTag(Main.MOD_ID + ":wall_candelabras");
+        public static final ITag.INamedTag<Block> HANGING_CANDELABRAS = BlockTags.makeWrapperTag(Main.MOD_ID + ":hanging_candelabras");
     }
 }
