14e8e01ed71eeba637ba0a68efed651f906c658b
stal111 <stal111@gmx.de>
2020-07-20 22:05:29 +0200

    Fix Edelwood Bucket Tooltip

--- src/main/java/com/stal111/forbidden_arcanus/event/TooltipListener.java
+++ src/main/java/com/stal111/forbidden_arcanus/event/TooltipListener.java
@@ -1,63 +1,52 @@
 package com.stal111.forbidden_arcanus.event;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.stal111.forbidden_arcanus.Main;
-import com.stal111.forbidden_arcanus.init.ModEnchantments;
 import com.stal111.forbidden_arcanus.init.ModItems;
 import com.stal111.forbidden_arcanus.item.EdelwoodFishBucketItem;
 import com.stal111.forbidden_arcanus.item.ICapacityBucket;
-import com.stal111.forbidden_arcanus.util.RenderUtils;
 import com.stal111.forbidden_arcanus.util.TooltipUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.AbstractGui;
-import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderTooltipEvent;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber
+@Mod.EventBusSubscriber(value = Dist.CLIENT)
 public class TooltipListener {
 
     @SubscribeEvent
     public static void onRenderTooltipPostText(RenderTooltipEvent.PostText event) {
         ItemStack stack = event.getStack();
-
-        Minecraft.getInstance().getItemRenderer().renderItemAndEffectIntoGUI(new ItemStack(ModItems.EDELWOOD_BUCKET.get()), event.getX(), event.getY());
+        ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
 
         if (stack.getItem() instanceof ICapacityBucket && !(stack.getItem() instanceof EdelwoodFishBucketItem)) {
             int capacity = ((ICapacityBucket) stack.getItem()).getCapacity();
             int fullness = ICapacityBucket.getFullness(stack);
 
-            System.out.println(capacity);
-
-                //RenderSystem.color3f(1F, 1F, 1F);
-                int y = TooltipUtils.shiftTextByLines(event.getLines(), event.getY() + 11);
+            if (capacity > 0) {
+                RenderSystem.pushMatrix();
+                RenderSystem.color3f(1F, 1F, 1F);
+                int y = TooltipUtils.shiftTextByLines(event.getLines(), event.getY() + 12);
 
                 for (int i = 0; i < fullness; i++) {
                     int x = event.getX() + i * 14 - 2;
 
-                    Minecraft.getInstance().getItemRenderer().renderItemAndEffectIntoGUI(stack, x, y);
+                    itemRenderer.zLevel += 500;
+                    itemRenderer.renderItemAndEffectIntoGUI(stack, x, y);
+                    itemRenderer.zLevel -= 500;
                 }
                 for (int i = 0; i < (capacity - fullness); i++) {
                     int x = event.getX() + i * 14 + (fullness * 14 - 2);
 
-                    Minecraft.getInstance().getItemRenderer().renderItemAndEffectIntoGUI(new ItemStack(ModItems.EDELWOOD_BUCKET.get()), x, y);
-                   // renderItemTexture(event.getMatrixStack(), x, y, new ItemStack(ModItems.EDELWOOD_BUCKET.get()));
+                    itemRenderer.zLevel += 500;
+                    itemRenderer.renderItemAndEffectIntoGUI(new ItemStack(ModItems.EDELWOOD_BUCKET.get()), x, y);
+                    itemRenderer.zLevel -= 500;
+
                 }
-
+                RenderSystem.popMatrix();
+            }
         }
-    }
-
-    public static void renderItemTexture(MatrixStack matrixStack, int x, int y, ItemStack stack) {
-        Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation(Main.MOD_ID, "textures/item/" + stack.getItem().getRegistryName().getPath() + ".png"));
-        AbstractGui.blit(matrixStack, x, y, 1, 0, 0, 16, 16, 16, 16);
     }
 }
