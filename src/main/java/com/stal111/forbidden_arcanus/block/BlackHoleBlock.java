319e59be01907756cead70c97bea1e2eaffe1f3e
stal111 <stal111@gmx.de>
2020-08-17 21:50:30 +0200

    Black Hole Particle Effect

--- src/main/java/com/stal111/forbidden_arcanus/block/BlackHoleBlock.java
+++ src/main/java/com/stal111/forbidden_arcanus/block/BlackHoleBlock.java
@@ -3,13 +3,16 @@ package com.stal111.forbidden_arcanus.block;
 import com.stal111.forbidden_arcanus.tile.BlackHoleTileEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
+import java.util.Random;
 
 public class BlackHoleBlock extends Block {
 
@@ -33,5 +36,21 @@ public class BlackHoleBlock extends Block {
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
         return SHAPE;
+    }
+
+    @Override
+    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+        for(int i = 0; i < 3; ++i) {
+            int j = rand.nextInt(2) * 2 - 1;
+            int k = rand.nextInt(2) * 2 - 1;
+            double d0 = pos.getX() + 0.5D + 0.25D * j;
+            double d1 = pos.getY() + 0.5D;
+            double d2 = pos.getZ() + 0.5D + 0.25D * k;
+            double d3 = rand.nextFloat() * j;
+            double d4 = (rand.nextFloat() - 0.5D) * 0.125D;
+            double d5 = rand.nextFloat() * k;
+            worldIn.addParticle(ParticleTypes.PORTAL, d0, d1, d2, d3, d4, d5);
+        }
+
     }
 }
