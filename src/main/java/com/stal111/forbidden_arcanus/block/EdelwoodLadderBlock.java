8d1f64a907dfd6636df1f344ee79d5cdf6aa7035
stal111 <stal111@gmx.de>
2021-05-18 16:05:56 +0200

    Fix Edelwood Ladder not working

--- src/main/java/com/stal111/forbidden_arcanus/block/EdelwoodLadderBlock.java
+++ src/main/java/com/stal111/forbidden_arcanus/block/EdelwoodLadderBlock.java
@@ -1,16 +1,19 @@
 package com.stal111.forbidden_arcanus.block;
 
 import com.stal111.forbidden_arcanus.ForbiddenArcanus;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.LadderBlock;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraft.world.IWorldReader;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.List;
 
@@ -20,9 +23,13 @@ public class EdelwoodLadderBlock extends LadderBlock {
 		super(builder);
 	}
 
-	@OnlyIn(Dist.CLIENT)
 	@Override
-	public void addInformation(ItemStack stack, @Nullable IBlockReader world, List<ITextComponent> list, ITooltipFlag flag) {
+	public boolean isLadder(BlockState state, IWorldReader world, BlockPos pos, LivingEntity entity) {
+		return true;
+	}
+
+	@Override
+	public void addInformation(@Nonnull ItemStack stack, @Nullable IBlockReader world, List<ITextComponent> list, @Nonnull ITooltipFlag flag) {
 		list.add(new TranslationTextComponent("tooltip." + ForbiddenArcanus.MOD_ID + ".edelwood_ladder").mergeStyle(TextFormatting.GRAY));
 		super.addInformation(stack, world, list, flag);
 	}
--- src/main/java/com/stal111/forbidden_arcanus/data/server/tags/ModBlockTagsProvider.java
+++ src/main/java/com/stal111/forbidden_arcanus/data/server/tags/ModBlockTagsProvider.java
@@ -1,6 +1,7 @@
 package com.stal111.forbidden_arcanus.data.server.tags;
 
 import com.stal111.forbidden_arcanus.ForbiddenArcanus;
+import com.stal111.forbidden_arcanus.init.ModBlocks;
 import com.stal111.forbidden_arcanus.init.NewerModBlocks;
 import com.stal111.forbidden_arcanus.util.ModTags;
 import net.minecraft.data.BlockTagsProvider;
@@ -27,18 +28,18 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
 
     @Override
     protected void registerTags() {
-        getOrCreateBuilder(ModTags.Blocks.FUNGYSS_STEMS).add(NewerModBlocks.FUNGYSS_STEM.get(), NewerModBlocks.FUNGYSS_HYPHAE.get());
-        getOrCreateBuilder(BlockTags.LOGS).addTag(ModTags.Blocks.FUNGYSS_STEMS);
-        getOrCreateBuilder(BlockTags.PLANKS).add(NewerModBlocks.FUNGYSS_PLANKS.get());
-        getOrCreateBuilder(BlockTags.WOODEN_SLABS).add(NewerModBlocks.FUNGYSS_SLAB.get());
-        getOrCreateBuilder(BlockTags.WOODEN_STAIRS).add(NewerModBlocks.FUNGYSS_STAIRS.get());
-        getOrCreateBuilder(BlockTags.WOODEN_PRESSURE_PLATES).add(NewerModBlocks.FUNGYSS_PRESSURE_PLATE.get());
-        getOrCreateBuilder(BlockTags.WOODEN_BUTTONS).add(NewerModBlocks.FUNGYSS_BUTTON.get());
-        getOrCreateBuilder(BlockTags.WOODEN_FENCES).add(NewerModBlocks.FUNGYSS_FENCE.get());
-        getOrCreateBuilder(Tags.Blocks.FENCES_WOODEN).add(NewerModBlocks.FUNGYSS_FENCE.get());
-        getOrCreateBuilder(BlockTags.FENCE_GATES).add(NewerModBlocks.FUNGYSS_FENCE_GATE.get());
-        getOrCreateBuilder(Tags.Blocks.FENCE_GATES_WOODEN).add(NewerModBlocks.FUNGYSS_FENCE_GATE.get());
-        getOrCreateBuilder(BlockTags.UNSTABLE_BOTTOM_CENTER).add(NewerModBlocks.FUNGYSS_FENCE_GATE.get());
-
+        this.getOrCreateBuilder(ModTags.Blocks.FUNGYSS_STEMS).add(NewerModBlocks.FUNGYSS_STEM.get(), NewerModBlocks.FUNGYSS_HYPHAE.get());
+        this.getOrCreateBuilder(BlockTags.LOGS).addTag(ModTags.Blocks.FUNGYSS_STEMS);
+        this.getOrCreateBuilder(BlockTags.PLANKS).add(NewerModBlocks.FUNGYSS_PLANKS.get());
+        this.getOrCreateBuilder(BlockTags.WOODEN_SLABS).add(NewerModBlocks.FUNGYSS_SLAB.get());
+        this.getOrCreateBuilder(BlockTags.WOODEN_STAIRS).add(NewerModBlocks.FUNGYSS_STAIRS.get());
+        this.getOrCreateBuilder(BlockTags.WOODEN_PRESSURE_PLATES).add(NewerModBlocks.FUNGYSS_PRESSURE_PLATE.get());
+        this.getOrCreateBuilder(BlockTags.WOODEN_BUTTONS).add(NewerModBlocks.FUNGYSS_BUTTON.get());
+        this.getOrCreateBuilder(BlockTags.WOODEN_FENCES).add(NewerModBlocks.FUNGYSS_FENCE.get());
+        this.getOrCreateBuilder(Tags.Blocks.FENCES_WOODEN).add(NewerModBlocks.FUNGYSS_FENCE.get());
+        this.getOrCreateBuilder(BlockTags.FENCE_GATES).add(NewerModBlocks.FUNGYSS_FENCE_GATE.get());
+        this.getOrCreateBuilder(Tags.Blocks.FENCE_GATES_WOODEN).add(NewerModBlocks.FUNGYSS_FENCE_GATE.get());
+        this.getOrCreateBuilder(BlockTags.UNSTABLE_BOTTOM_CENTER).add(NewerModBlocks.FUNGYSS_FENCE_GATE.get());
+        this.getOrCreateBuilder(BlockTags.CLIMBABLE).add(ModBlocks.EDELWOOD_LADDER.getBlock());
     }
 }
