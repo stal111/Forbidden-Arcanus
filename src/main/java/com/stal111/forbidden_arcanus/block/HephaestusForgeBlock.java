faca747bd08effbd4715ecc0b15f0dc58753bc1e
stal111 <stal111@gmx.de>
2021-06-12 15:24:01 +0200

    Make Hephaestus Forge Waterloggable

--- src/main/java/com/stal111/forbidden_arcanus/block/HephaestusForgeBlock.java
+++ src/main/java/com/stal111/forbidden_arcanus/block/HephaestusForgeBlock.java
@@ -2,15 +2,25 @@ package com.stal111.forbidden_arcanus.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.IWaterLoggable;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 import net.valhelsia.valhelsia_core.helper.VoxelShapeHelper;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * Hephaestus Forge Block
@@ -19,7 +29,9 @@ import javax.annotation.Nonnull;
  * @author stal111
  * @version 2.0.0
  */
-public class HephaestusForgeBlock extends Block {
+public class HephaestusForgeBlock extends Block implements IWaterLoggable {
+
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     private static final VoxelShape SHAPE = VoxelShapes.combineAndSimplify(
             VoxelShapeHelper.combineAll(
@@ -37,11 +49,38 @@ public class HephaestusForgeBlock extends Block {
 
     public HephaestusForgeBlock(Properties properties) {
         super(properties);
+        this.setDefaultState(this.getStateContainer().getBaseState().with(WATERLOGGED, false));
     }
 
     @Nonnull
     @Override
     public VoxelShape getShape(@Nonnull BlockState state, @Nonnull IBlockReader world, @Nonnull BlockPos pos, @Nonnull ISelectionContext context) {
         return SHAPE;
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(@Nonnull BlockItemUseContext context) {
+        return this.getDefaultState().with(WATERLOGGED, context.getWorld().getFluidState(context.getPos()).getFluid() == Fluids.WATER);
+    }
+
+    @Nonnull
+    @Override
+    public BlockState updatePostPlacement(@Nonnull BlockState state, @Nonnull Direction facing, @Nonnull BlockState facingState, @Nonnull IWorld world, @Nonnull BlockPos currentPos, @Nonnull BlockPos facingPos) {
+        if (state.get(WATERLOGGED)) {
+            world.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+        return super.updatePostPlacement(state, facing, facingState, world, currentPos, facingPos);
+    }
+
+    @Nonnull
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(WATERLOGGED);
     }
 }
