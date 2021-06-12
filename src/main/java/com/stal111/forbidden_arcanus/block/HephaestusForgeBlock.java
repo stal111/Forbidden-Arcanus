a51955f7608d1a6ef5fa3b599710425e9918bfd3
stal111 <stal111@gmx.de>
2021-06-12 15:07:52 +0200

    Update Hephaestus Forge Shape

--- src/main/java/com/stal111/forbidden_arcanus/block/HephaestusForgeBlock.java
+++ src/main/java/com/stal111/forbidden_arcanus/block/HephaestusForgeBlock.java
@@ -3,24 +3,37 @@ package com.stal111.forbidden_arcanus.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.valhelsia.valhelsia_core.helper.VoxelShapeHelper;
 
 import javax.annotation.Nonnull;
 
+/**
+ * Hephaestus Forge Block
+ * Forbidden Arcanus - com.stal111.forbidden_arcanus.block.HephaestusForgeBlock
+ *
+ * @author stal111
+ * @version 2.0.0
+ */
 public class HephaestusForgeBlock extends Block {
 
-    private static final VoxelShape SHAPE = VoxelShapeHelper.combineAll(
-            Block.makeCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 2.0D, 14.0D),
-            Block.makeCuboidShape(3.0D, 2.0D, 3.0D, 13.0D, 3.0D, 13.0D),
-            Block.makeCuboidShape(5.0D, 3.0D, 5.0D, 11.0D, 7.0D, 11.0D),
-            Block.makeCuboidShape(1.0D, 7.0D, 1.0D, 15.0D, 13.0D, 15.0D),
-            Block.makeCuboidShape(0.0D, 11.0D, 0.0D, 3.0D, 14.0D, 3.0D),
-            Block.makeCuboidShape(0.0D, 11.0D, 13.0D, 3.0D, 14.0D, 16.0D),
-            Block.makeCuboidShape(13.0D, 11.0D, 0.0D, 16.0D, 14.0D, 3.0D),
-            Block.makeCuboidShape(13.0D, 11.0D, 13.0D, 16.0D, 14.0D, 16.0D));
+    private static final VoxelShape SHAPE = VoxelShapes.combineAndSimplify(
+            VoxelShapeHelper.combineAll(
+                    makeCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 3.0D, 15.0D),
+                    makeCuboidShape(2.0D, 3.0D, 2.0D, 14.0D, 4.0D, 14.0D),
+                    makeCuboidShape(4.0D, 4.0D, 4.0D, 12.0D, 8.0D, 12.0D),
+                    makeCuboidShape(0.0D, 8.0D, 0.0D, 16.0D, 16.0D, 16.0D)
+            ),
+            VoxelShapeHelper.combineAll(
+                    makeCuboidShape(0.0D, 15.0D, 3.0D, 16.0D, 16.0D, 13.0D),
+                    makeCuboidShape(3.0D, 15.0D, 0.0D, 13.0D, 16.0D, 16.0D)
+            ),
+            IBooleanFunction.ONLY_FIRST
+    );
 
     public HephaestusForgeBlock(Properties properties) {
         super(properties);
