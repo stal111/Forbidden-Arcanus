55e0fed14ab88c0769e77f175c6c861d48821a73
stal111 <stal111@gmx.de>
2021-07-09 15:33:54 +0200

    Cleanup MysticalDaggerItem.java

--- src/main/java/com/stal111/forbidden_arcanus/item/MysticalDaggerItem.java
+++ src/main/java/com/stal111/forbidden_arcanus/item/MysticalDaggerItem.java
@@ -2,9 +2,9 @@ package com.stal111.forbidden_arcanus.item;
 
 import com.stal111.forbidden_arcanus.ForbiddenArcanus;
 import com.stal111.forbidden_arcanus.init.ModEffects;
-import com.stal111.forbidden_arcanus.init.ModItems;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -14,12 +14,17 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
+import javax.annotation.Nonnull;
 import java.util.List;
-import java.util.Random;
 
+/**
+ * Mystical Dagger Item
+ * Forbidden Arcanus - com.stal111.forbidden_arcanus.item.MysticalDaggerItem
+ *
+ * @author stal111
+ * @version 2.0.0
+ */
 public class MysticalDaggerItem extends SwordItem {
 
     public MysticalDaggerItem(IItemTier tier, float attackDamage, float attackSpeed, Item.Properties properties) {
@@ -33,24 +38,22 @@ public class MysticalDaggerItem extends SwordItem {
 
     @Override
     public ItemStack getContainerItem(ItemStack stack) {
-        ItemStack stack1 = new ItemStack(ModItems.MYSTICAL_DAGGER.get());
-        stack1.setDamage(stack.getDamage() + 10);
-        return stack1;
+        ItemStack copy = stack.copy();
+        copy.setDamage(stack.getDamage() + 10);
+        return copy;
     }
 
     @Override
-    public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-        int i = new Random().nextInt(5);
-        if (i == 1) {
+    public boolean hitEntity(@Nonnull ItemStack stack, @Nonnull LivingEntity target, @Nonnull LivingEntity attacker) {
+        if (attacker.getRNG().nextInt(5) == 0) {
             target.addPotionEffect(new EffectInstance(ModEffects.DARKENED.get(), 100, 0));
         }
         return super.hitEntity(stack, target, attacker);
     }
 
-    @OnlyIn(Dist.CLIENT)
     @Override
-    public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
-        super.addInformation(stack, worldIn, tooltip, flagIn);
-        tooltip.add((new TranslationTextComponent("tooltip." + ForbiddenArcanus.MOD_ID + ".mystical_dagger")).mergeStyle(TextFormatting.GRAY));
+    public void addInformation(@Nonnull ItemStack stack, World worldIn, @Nonnull List<ITextComponent> tooltip, @Nonnull ITooltipFlag flag) {
+        super.addInformation(stack, worldIn, tooltip, flag);
+        tooltip.add(new TranslationTextComponent("tooltip." + ForbiddenArcanus.MOD_ID + ".mystical_dagger").mergeStyle(TextFormatting.GRAY));
     }
 }
