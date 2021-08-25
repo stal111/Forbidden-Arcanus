a426f5436deb18ffc6bd5e986ede6aef4a6b49c2
Jon McManus <due@wxwhatever.com>
2021-08-26 09:10:20 +1000

    Fix end removals.

--- src/main/java/com/stal111/forbidden_arcanus/event/WorldLoadListener.java
+++ src/main/java/com/stal111/forbidden_arcanus/event/WorldLoadListener.java
@@ -1,10 +1,8 @@
 package com.stal111.forbidden_arcanus.event;
 
 import com.mojang.serialization.Codec;
-import com.stal111.forbidden_arcanus.config.WorldGenConfig;
 import com.stal111.forbidden_arcanus.ForbiddenArcanus;
 import com.stal111.forbidden_arcanus.init.world.ModStructures;
-import net.minecraft.util.RegistryKey;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
@@ -18,6 +16,7 @@ import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import net.valhelsia.valhelsia_core.world.IValhelsiaStructure;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
@@ -35,46 +34,50 @@ import java.util.Map;
  */
 @Mod.EventBusSubscriber
 public class WorldLoadListener {
-    private static MethodHandle GETCODEC_METHOD = null;
+  private static MethodHandle GETCODEC_METHOD = null;
 
-    @SubscribeEvent
-    public static void onWorldLoad(WorldEvent.Load event) {
-        if (event.getWorld() instanceof ServerWorld) {
-            ServerWorld serverWorld = (ServerWorld) event.getWorld();
+  @SubscribeEvent
+  public static void onWorldLoad(WorldEvent.Load event) {
+    if (event.getWorld() instanceof ServerWorld) {
+      ServerWorld serverWorld = (ServerWorld) event.getWorld();
 
-            if (serverWorld.getChunkProvider().getChunkGenerator() instanceof FlatChunkGenerator && serverWorld.getDimensionKey().equals(World.OVERWORLD)) {
-                return;
-            }
+      if (serverWorld.getChunkProvider().getChunkGenerator() instanceof FlatChunkGenerator && serverWorld.getDimensionKey().equals(World.OVERWORLD)) {
+        return;
+      }
 
-            if (GETCODEC_METHOD == null) {
-                Method codec = ObfuscationReflectionHelper.findMethod(ChunkGenerator.class, "func_230347_a_");
-                MethodHandles.Lookup l = MethodHandles.lookup();
-                try {
-                    GETCODEC_METHOD = l.unreflect(codec);
-                } catch (IllegalAccessException e) {
-                    ForbiddenArcanus.LOGGER.error("Unable to unreflect codec getter.", e);
-                    return;
-                }
-            }
-
-            ResourceLocation chunkGen = null;
-            try {
-                //noinspection unchecked
-                chunkGen = Registry.CHUNK_GENERATOR_CODEC.getKey((Codec<? extends ChunkGenerator>) GETCODEC_METHOD.invokeExact(serverWorld.getChunkProvider().generator));
-            } catch (Throwable throwable) {
-                ForbiddenArcanus.LOGGER.error("Unable to look up chunk provider's generator", throwable);
-                return;
-            }
-            if (chunkGen != null && chunkGen.getNamespace().equals("terraforrged")) {
-                return;
-            }
-
-            if (serverWorld.getDimensionKey().equals(World.OVERWORLD)) {
-                Map<Structure<?>, StructureSeparationSettings> tempMap = new HashMap<>(serverWorld.getChunkProvider().generator.func_235957_b_().func_236195_a_());
-                ModStructures.MOD_STRUCTURES.forEach(structure -> tempMap.putIfAbsent(structure.getStructure(), DimensionStructuresSettings.field_236191_b_.get(structure.getStructure())));
-
-                serverWorld.getChunkProvider().generator.func_235957_b_().field_236193_d_ = tempMap;
-            }
+      if (GETCODEC_METHOD == null) {
+        Method codec = ObfuscationReflectionHelper.findMethod(ChunkGenerator.class, "func_230347_a_");
+        MethodHandles.Lookup l = MethodHandles.lookup();
+        try {
+          GETCODEC_METHOD = l.unreflect(codec);
+        } catch (IllegalAccessException e) {
+          ForbiddenArcanus.LOGGER.error("Unable to unreflect codec getter.", e);
+          return;
         }
+      }
+
+      ResourceLocation chunkGen = null;
+      try {
+        //noinspection unchecked
+        chunkGen = Registry.CHUNK_GENERATOR_CODEC.getKey((Codec<? extends ChunkGenerator>) GETCODEC_METHOD.invokeExact(serverWorld.getChunkProvider().generator));
+      } catch (Throwable throwable) {
+        ForbiddenArcanus.LOGGER.error("Unable to look up chunk provider's generator", throwable);
+        return;
+      }
+      if (chunkGen != null && chunkGen.getNamespace().equals("terraforrged")) {
+        return;
+      }
+
+      Map<Structure<?>, StructureSeparationSettings> tempMap = new HashMap<>(serverWorld.getChunkProvider().generator.func_235957_b_().func_236195_a_());
+      for (IValhelsiaStructure structure : ModStructures.MOD_STRUCTURES) {
+        if (serverWorld.getDimensionKey().equals(World.OVERWORLD)) {
+          tempMap.putIfAbsent(structure.getStructure(), DimensionStructuresSettings.field_236191_b_.get(structure.getStructure()));
+        } else {
+          tempMap.remove(structure.getStructure());
+        }
+      }
+
+      serverWorld.getChunkProvider().generator.func_235957_b_().field_236193_d_ = tempMap;
     }
+  }
 }
