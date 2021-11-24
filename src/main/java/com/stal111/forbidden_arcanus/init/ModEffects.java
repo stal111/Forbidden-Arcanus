57527da85635e6688b3fddcf2b7a49b87072110b
stal111 <stal111@gmx.de>
2021-11-24 20:58:59 +0100

    Remove Fly Effect

--- src/main/java/com/stal111/forbidden_arcanus/init/ModEffects.java
+++ src/main/java/com/stal111/forbidden_arcanus/init/ModEffects.java
@@ -2,7 +2,6 @@ package com.stal111.forbidden_arcanus.init;
 
 import com.stal111.forbidden_arcanus.ForbiddenArcanus;
 import com.stal111.forbidden_arcanus.effect.DarkenedEffect;
-import com.stal111.forbidden_arcanus.effect.FlyEffect;
 import com.stal111.forbidden_arcanus.effect.SpectralEyeEffect;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -16,7 +15,6 @@ public class ModEffects {
 
     public static final DeferredRegister<MobEffect> EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, ForbiddenArcanus.MOD_ID);
 
-    public static final RegistryObject<MobEffect> FLY = register("fly", new FlyEffect(MobEffectCategory.BENEFICIAL, 745784));
     public static final RegistryObject<MobEffect> SPECTRAL_VISION = register("spectral_vision", new SpectralEyeEffect(MobEffectCategory.BENEFICIAL, 745784));
     public static final RegistryObject<MobEffect> DARKENED = register("darkened", new DarkenedEffect(MobEffectCategory.HARMFUL,74578).addAttributeModifier(Attributes.MOVEMENT_SPEED, "7107DE5E-7CE8-4030-940E-514C1F160890", (double)-0.15F, AttributeModifier.Operation.MULTIPLY_TOTAL));
 
