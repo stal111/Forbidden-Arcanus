24d021fa069d2e35bc5627ecb0051c4f135e1adb
stal111 <stal111@gmx.de>
2021-12-26 19:09:02 +0100

    Update ItemStackMixin.java

--- src/main/java/com/stal111/forbidden_arcanus/core/mixin/ItemStackMixin.java
+++ src/main/java/com/stal111/forbidden_arcanus/core/mixin/ItemStackMixin.java
@@ -4,15 +4,12 @@ import com.stal111.forbidden_arcanus.common.item.modifier.ItemModifier;
 import com.stal111.forbidden_arcanus.common.item.modifier.ModifierHelper;
 import com.stal111.forbidden_arcanus.core.init.other.ModItemModifiers;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Random;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
@@ -22,8 +19,8 @@ public abstract class ItemStackMixin {
 
     @Shadow public abstract boolean hasCustomHoverName();
 
-    @Inject(at = @At(value = "HEAD"), method = "hurt", cancellable = true)
-    private void forbiddenArcanus_hurt(int i, Random random, ServerPlayer player, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/item/ItemStack;getTag()Lnet/minecraft/nbt/CompoundTag;"), method = "isDamageableItem", cancellable = true)
+    private void forbiddenArcanus_isDamageableItem(CallbackInfoReturnable<Boolean> cir) {
         if (ModifierHelper.getModifier(this.copy()) == ModItemModifiers.ETERNAL.get()) {
             cir.setReturnValue(false);
         }
