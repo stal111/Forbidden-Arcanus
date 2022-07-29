c6afede77401a6332a80a59d1b7c39312fd49f52
stal111 <stal111@gmx.de>
2022-07-29 14:11:28 +0200

    Remove unnecessary stack.copy()
    Should improve performance a bit & fix #271

--- src/main/java/com/stal111/forbidden_arcanus/core/mixin/ItemStackMixin.java
+++ src/main/java/com/stal111/forbidden_arcanus/core/mixin/ItemStackMixin.java
@@ -14,21 +14,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
 
-    @Shadow
-    public abstract ItemStack copy();
-
     @Shadow public abstract boolean hasCustomHoverName();
 
     @Inject(at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/item/ItemStack;getTag()Lnet/minecraft/nbt/CompoundTag;"), method = "isDamageableItem", cancellable = true)
     private void forbiddenArcanus_isDamageableItem(CallbackInfoReturnable<Boolean> cir) {
-        if (ModifierHelper.getModifier(this.copy()) == ModItemModifiers.ETERNAL.get()) {
+        if (ModifierHelper.getModifier((ItemStack) (Object) this) == ModItemModifiers.ETERNAL.get()) {
             cir.setReturnValue(false);
         }
     }
 
     @Inject(at = @At(value = "RETURN"), method = "getHoverName", cancellable = true)
     private void forbiddenArcanus_getHoverName(CallbackInfoReturnable<Component> cir) {
-        ItemModifier modifier = ModifierHelper.getModifier(this.copy());
+        ItemModifier modifier = ModifierHelper.getModifier((ItemStack) (Object) this);
 
         if (!this.hasCustomHoverName() && modifier != null) {
             cir.setReturnValue(modifier.getComponent().append(" ").append(cir.getReturnValue()));
