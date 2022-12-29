30a77ad1a974386e9e16db425d57f87cbaaa6b1c
stal111 <stal111@gmx.de>
2022-12-29 16:05:31 +0100

    Fix saplings not working

--- src/main/java/com/stal111/forbidden_arcanus/common/block/grower/CherrywoodTreeGrower.java
+++ src/main/java/com/stal111/forbidden_arcanus/common/block/grower/CherrywoodTreeGrower.java
@@ -1,28 +1,25 @@
 package com.stal111.forbidden_arcanus.common.block.grower;
 
+import com.stal111.forbidden_arcanus.core.init.world.ModConfiguredFeatures;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.grower.AbstractTreeGrower;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Cherrywood Tree Grower <br>
  * Forbidden Arcanus - com.stal111.forbidden_arcanus.common.block.grower.CherrywoodTreeGrower
  *
  * @author stal111
- * @version 1.19 - 2.1.0
  * @since 2021-11-23
  */
 public class CherrywoodTreeGrower extends AbstractTreeGrower {
 
     @Nullable
     @Override
-    protected ResourceKey<ConfiguredFeature<?, ?>> getConfiguredFeature(@Nonnull RandomSource random, boolean largeHive) {
-        //TODO
-        return null;
-       // return random.nextDouble() < 0.3D ? ModConfiguredFeatures.LARGE_CHERRY.getHolder().get() : ModConfiguredFeatures.SMALL_CHERRY.getHolder().get();
+    protected ResourceKey<ConfiguredFeature<?, ?>> getConfiguredFeature(@NotNull RandomSource random, boolean largeHive) {
+        return random.nextDouble() < 0.3D ? ModConfiguredFeatures.LARGE_CHERRY : ModConfiguredFeatures.SMALL_CHERRY;
     }
 }
--- src/main/java/com/stal111/forbidden_arcanus/common/block/grower/MysterywoodTreeGrower.java
+++ src/main/java/com/stal111/forbidden_arcanus/common/block/grower/MysterywoodTreeGrower.java
@@ -1,28 +1,25 @@
 package com.stal111.forbidden_arcanus.common.block.grower;
 
+import com.stal111.forbidden_arcanus.core.init.world.ModConfiguredFeatures;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.grower.AbstractTreeGrower;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Mysterywood Tree Grower <br>
  * Forbidden Arcanus - com.stal111.forbidden_arcanus.common.block.grower.MysterywoodTreeGrower
  *
  * @author stal111
- * @version 1.19 - 2.1.0
  * @since 2021-11-23
  */
 public class MysterywoodTreeGrower extends AbstractTreeGrower {
 
     @Nullable
     @Override
-    protected ResourceKey<ConfiguredFeature<?, ?>> getConfiguredFeature(@Nonnull RandomSource random, boolean largeHive) {
-        //TODO
-        return null;
-        //return ModConfiguredFeatures.AURUM.getHolder().get();
+    protected ResourceKey<ConfiguredFeature<?, ?>> getConfiguredFeature(@NotNull RandomSource random, boolean largeHive) {
+        return ModConfiguredFeatures.AURUM;
     }
 }
