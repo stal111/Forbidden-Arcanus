1f1d1d7cdd6528a66e044e66dd4cd9116b4ca7fd
stal111 <stal111@gmx.de>
2022-12-29 13:32:30 +0100

    Datagen configured features

--- src/main/java/com/stal111/forbidden_arcanus/ModRegistries.java
+++ src/main/java/com/stal111/forbidden_arcanus/ModRegistries.java
@@ -4,12 +4,10 @@ import com.google.common.collect.ImmutableList;
 import com.stal111.forbidden_arcanus.core.init.*;
 import com.stal111.forbidden_arcanus.core.init.other.ModActivities;
 import com.stal111.forbidden_arcanus.core.init.other.ModLootModifiers;
-import com.stal111.forbidden_arcanus.core.init.world.ModStructurePieces;
-import com.stal111.forbidden_arcanus.core.init.world.ModStructureSets;
-import com.stal111.forbidden_arcanus.core.init.world.ModStructureTypes;
-import com.stal111.forbidden_arcanus.core.init.world.ModStructures;
+import com.stal111.forbidden_arcanus.core.init.world.*;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureSet;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -38,9 +36,10 @@ public class ModRegistries extends RegistryCollector {
         this.addMappedHelper(Registries.PARTICLE_TYPE, ModParticles::new);
         this.addMappedHelper(Registries.MEMORY_MODULE_TYPE, ModMemoryModules::new);
         this.addMappedHelper(Registries.ACTIVITY, ModActivities::new);
+        this.addMappedHelper(Registries.TRUNK_PLACER_TYPE, ModTrunkPlacers::new);
 
         this.addDatapackHelper(Registries.STRUCTURE, (dataProviderInfo, bootstapContext) -> ImmutableList.of(new ModStructures(dataProviderInfo, (BootstapContext<Structure>) bootstapContext)));
         this.addDatapackHelper(Registries.STRUCTURE_SET, (dataProviderInfo, bootstapContext) -> ImmutableList.of(new ModStructureSets(dataProviderInfo, (BootstapContext<StructureSet>) bootstapContext)));
-
+        this.addDatapackHelper(Registries.CONFIGURED_FEATURE, (dataProviderInfo, bootstapContext) -> ImmutableList.of(new ModConfiguredFeatures(dataProviderInfo, (BootstapContext<ConfiguredFeature<?, ?>>) bootstapContext)));
     }
 }
