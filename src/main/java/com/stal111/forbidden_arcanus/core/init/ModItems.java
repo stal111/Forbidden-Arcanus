96c6c89ea443a593e1473fdd3e63330363663617
stal111 <stal111@gmx.de>
2023-01-02 17:39:24 +0100

    Add Ferrognetic Mixture lang entry

--- src/main/java/com/stal111/forbidden_arcanus/core/init/ModItems.java
+++ src/main/java/com/stal111/forbidden_arcanus/core/init/ModItems.java
@@ -64,7 +64,7 @@ public class ModItems implements RegistryClass {
     public static final RegistryObject<Item> GOLDEN_FEATHER = HELPER.register("golden_feather", () -> new Item(new Item.Properties().tab(ForbiddenArcanus.FORBIDDEN_ARCANUS)));
     public static final RegistryObject<Item> EDELWOOD_STICK = HELPER.register("edelwood_stick", () -> new Item(new Item.Properties().tab(ForbiddenArcanus.FORBIDDEN_ARCANUS)));
     public static final RegistryObject<Item> WAX = HELPER.register("wax", () -> new Item(new Item.Properties().tab(ForbiddenArcanus.FORBIDDEN_ARCANUS)));
-     public static final RegistryObject<FerrogneticMixtureItem> FERROGNETIC_MIXTURE = HELPER.register("ferrognetic_mixture", () -> new FerrogneticMixtureItem(new Item.Properties()));
+     public static final RegistryObject<FerrogneticMixtureItem> FERROGNETIC_MIXTURE = HELPER.register("ferrognetic_mixture", () -> new FerrogneticMixtureItem(new Item.Properties().tab(ForbiddenArcanus.FORBIDDEN_ARCANUS));
     public static final RegistryObject<Item> SPAWNER_SCRAP = HELPER.register("spawner_scrap", () -> new Item(new Item.Properties().tab(ForbiddenArcanus.FORBIDDEN_ARCANUS)));
     public static final RegistryObject<Item> QUANTUM_CATCHER = HELPER.register("quantum_catcher", () -> new QuantumCatcherItem(new Item.Properties().tab(ForbiddenArcanus.FORBIDDEN_ARCANUS)));
 
