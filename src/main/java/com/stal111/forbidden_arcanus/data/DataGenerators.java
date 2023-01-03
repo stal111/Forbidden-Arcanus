10315c28d75f82211e2d3ff69cba89ff30ea3f91
stal111 <stal111@gmx.de>
2023-01-03 16:30:07 +0100

    Datagen particle jsons

--- src/main/java/com/stal111/forbidden_arcanus/data/DataGenerators.java
+++ src/main/java/com/stal111/forbidden_arcanus/data/DataGenerators.java
@@ -4,6 +4,7 @@ import com.stal111.forbidden_arcanus.ForbiddenArcanus;
 import com.stal111.forbidden_arcanus.data.client.ModBlockStateProvider;
 import com.stal111.forbidden_arcanus.data.client.ModItemModelProvider;
 import com.stal111.forbidden_arcanus.data.client.ModSoundsProvider;
+import com.stal111.forbidden_arcanus.data.particle.ParticleDataProvider;
 import com.stal111.forbidden_arcanus.data.recipes.ApplyModifierRecipeProvider;
 import com.stal111.forbidden_arcanus.data.recipes.ClibanoRecipeProvider;
 import com.stal111.forbidden_arcanus.data.recipes.CraftingRecipeProvider;
@@ -46,6 +47,7 @@ public class DataGenerators {
         generator.addProvider(event.includeClient(), new ModItemModelProvider(info));
 
         generator.addProvider(event.includeClient(), new ModSoundsProvider(info));
+        generator.addProvider(event.includeServer(), new ParticleDataProvider(info));
 
         // Server Providers
         ModBlockTagsProvider blockTagsProvider = new ModBlockTagsProvider(info);
