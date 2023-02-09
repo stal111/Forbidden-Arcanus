26759fa2be25468e4606dc53de9adc444e2cecf3
stal111 <stal111@gmx.de>
2023-02-09 17:37:36 +0100

    Add Ferrognetic Mixture ritual

--- src/main/java/com/stal111/forbidden_arcanus/common/block/entity/forge/ritual/BuiltinRituals.java
+++ src/main/java/com/stal111/forbidden_arcanus/common/block/entity/forge/ritual/BuiltinRituals.java
@@ -11,6 +11,7 @@ public class BuiltinRituals {
 
     public static final ResourceLocation ETERNAL_STELLA = new ResourceLocation(ForbiddenArcanus.MOD_ID, "eternal_stella");
     public static final ResourceLocation SMELTER_PRISM = new ResourceLocation(ForbiddenArcanus.MOD_ID, "smelter_prism");
+    public static final ResourceLocation FERROGNETIC_MIXTURE = new ResourceLocation(ForbiddenArcanus.MOD_ID, "ferrognetic_mixture");
     public static final ResourceLocation UPGRADE_TIER_2 = new ResourceLocation(ForbiddenArcanus.MOD_ID, "upgrade_tier_2");
     public static final ResourceLocation UPGRADE_TIER_3 = new ResourceLocation(ForbiddenArcanus.MOD_ID, "upgrade_tier_3");
     public static final ResourceLocation UPGRADE_TIER_4 = new ResourceLocation(ForbiddenArcanus.MOD_ID, "upgrade_tier_4");
