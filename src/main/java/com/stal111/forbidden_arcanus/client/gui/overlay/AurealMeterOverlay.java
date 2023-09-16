251408259104e886d0d2efef2cdb9b8ae5f5e7bb
stal111 <stal111@gmx.de>
2023-09-16 22:47:05 +0200

    Change Aureal Bar to fill from right to left

--- src/main/java/com/stal111/forbidden_arcanus/client/gui/overlay/AurealMeterOverlay.java
+++ src/main/java/com/stal111/forbidden_arcanus/client/gui/overlay/AurealMeterOverlay.java
@@ -15,7 +15,10 @@ import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 import java.util.Collections;
 
 public class AurealMeterOverlay implements IGuiOverlay {
+
     private static final ResourceLocation TEXTURE = new ResourceLocation(ForbiddenArcanus.MOD_ID, "textures/gui/hud.png");
+
+    private static final int BAR_WIDTH = 75;
 
     @Override
     public void render(ForgeGui gui, GuiGraphics guiGraphics, float partialTick, int screenWidth, int screenHeight) {
@@ -27,14 +30,16 @@ public class AurealMeterOverlay implements IGuiOverlay {
         }
         IAureal aureal = AurealHelper.getCapability(player);
 
-        guiGraphics.blit(TEXTURE, window.getGuiScaledWidth() / 2 + 9, window.getGuiScaledHeight() - 25 - 23, 17, 9, 81, 10, 256, 128);
+        guiGraphics.blit(TEXTURE, window.getGuiScaledWidth() / 2 + 10, window.getGuiScaledHeight() - 25 - 23, 18, 9, 81, 9, 256, 128);
 
         this.renderOverlay(guiGraphics, window, aureal);
-        this.renderOverlay(guiGraphics, window, aureal);
     }
+
     private void renderOverlay(GuiGraphics guiGraphics, Window window, IAureal aureal) {
-        int xSize = Math.toIntExact(Math.round(80.0F * ((aureal.getAureal()) / 200.0F)));
-        guiGraphics.blit(TEXTURE, window.getGuiScaledWidth() / 2 + 11,window.getGuiScaledHeight() - 25 - 23, 19, 18,xSize, 18, 256, 128);
+        int xSize = Math.toIntExact(Math.round(BAR_WIDTH * ((aureal.getAureal()) / 200.0F)));
+        int startOffset = BAR_WIDTH - xSize;
+
+        guiGraphics.blit(TEXTURE, window.getGuiScaledWidth() / 2 + 13 + startOffset, window.getGuiScaledHeight() - 25 - 22, 21 + startOffset, 19, xSize, 7, 256, 128);
     }
 }
 
