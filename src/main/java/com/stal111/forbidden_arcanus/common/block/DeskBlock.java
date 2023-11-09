71320483bebe9bc70bb97feda653be2e338dab47
stal111 <stal111@gmx.de>
2023-11-09 15:10:55 +0100

    feat: allow desk to be turned into research desk

--- src/main/java/com/stal111/forbidden_arcanus/common/block/DeskBlock.java
+++ src/main/java/com/stal111/forbidden_arcanus/common/block/DeskBlock.java
@@ -1,9 +1,15 @@
 package com.stal111.forbidden_arcanus.common.block;
 
+import com.stal111.forbidden_arcanus.core.init.ModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
@@ -14,11 +20,13 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.valhelsia.valhelsia_core.api.common.helper.VoxelShapeHelper;
+import net.valhelsia.valhelsia_core.api.common.util.ItemStackUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -53,6 +61,25 @@ public class DeskBlock extends HorizontalDirectionalBlock implements SimpleWater
     @Override
     public VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context) {
         return this.shapesCache.get(state);
+    }
+
+    @Override
+    public @NotNull InteractionResult use(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hit) {
+        ItemStack stack = player.getItemInHand(hand);
+
+        if (stack.is(ModBlocks.FORBIDDENOMICON.get().asItem())) {
+            BlockState newState = ModBlocks.RESEARCH_DESK.get().defaultBlockState()
+                    .setValue(FACING, state.getValue(FACING))
+                    .setValue(WATERLOGGED, state.getValue(WATERLOGGED));
+
+            level.setBlockAndUpdate(pos, newState);
+
+            ItemStackUtils.shrinkStack(player, stack);
+
+            return InteractionResult.sidedSuccess(level.isClientSide());
+        }
+
+        return super.use(state, level, pos, player, hand, hit);
     }
 
     @Nullable
--- src/main/java/com/stal111/forbidden_arcanus/common/block/ResearchDeskBlock.java
+++ src/main/java/com/stal111/forbidden_arcanus/common/block/ResearchDeskBlock.java
@@ -2,7 +2,12 @@ package com.stal111.forbidden_arcanus.common.block;
 
 import com.stal111.forbidden_arcanus.common.block.entity.desk.ResearchDeskBlockEntity;
 import com.stal111.forbidden_arcanus.core.init.ModBlockEntities;
+import com.stal111.forbidden_arcanus.core.init.ModBlocks;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.EntityBlock;
@@ -10,6 +15,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -27,6 +33,31 @@ public class ResearchDeskBlock extends DeskBlock implements EntityBlock {
     @Override
     public BlockEntity newBlockEntity(@NotNull BlockPos pos, @NotNull BlockState state) {
         return new ResearchDeskBlockEntity(pos, state);
+    }
+
+    @Override
+    public @NotNull InteractionResult use(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hit) {
+        ItemStack stack = player.getItemInHand(hand);
+
+        if (player.isShiftKeyDown()) {
+            BlockState newState = ModBlocks.DESK.get().defaultBlockState()
+                    .setValue(FACING, state.getValue(FACING))
+                    .setValue(WATERLOGGED, state.getValue(WATERLOGGED));
+
+            level.setBlockAndUpdate(pos, newState);
+
+            ItemStack forbiddenomicon = new ItemStack(ModBlocks.FORBIDDENOMICON.get());
+
+            if (stack.isEmpty()) {
+                player.setItemInHand(hand, forbiddenomicon);
+            } else if (!player.addItem(forbiddenomicon)) {
+                player.drop(forbiddenomicon, false);
+            }
+
+            return InteractionResult.sidedSuccess(level.isClientSide());
+        }
+
+        return InteractionResult.PASS;
     }
 
     @Override
