71d7a2340440818c9bb6f8c7a7414cf173b0e4d6
stal111 <stal111@gmx.de>
2023-11-19 18:47:23 +0100

    refactor: remove old RitualInput serialization methods

--- src/main/java/com/stal111/forbidden_arcanus/common/block/entity/forge/ritual/RitualInput.java
+++ src/main/java/com/stal111/forbidden_arcanus/common/block/entity/forge/ritual/RitualInput.java
@@ -2,7 +2,6 @@ package com.stal111.forbidden_arcanus.common.block.entity.forge.ritual;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.item.crafting.Ingredient;
 
 import java.util.Optional;
@@ -13,7 +12,7 @@ import java.util.Optional;
  */
 public record RitualInput(Ingredient ingredient, int amount) {
 
-    public static final Codec<RitualInput> CODEC = RecordCodecBuilder.create((instance) -> instance.group(
+    public static final Codec<RitualInput> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Ingredient.CODEC_NONEMPTY.fieldOf("ingredient").forGetter(input -> {
                 return input.ingredient;
             }),
@@ -23,13 +22,4 @@ public record RitualInput(Ingredient ingredient, int amount) {
     ).apply(instance, (name, amount) -> {
         return new RitualInput(name, amount.orElse(1));
     }));
-
-    public static RitualInput fromNetwork(FriendlyByteBuf buffer) {
-        return new RitualInput(Ingredient.fromNetwork(buffer), buffer.readShort());
-    }
-
-    public void toNetwork(FriendlyByteBuf buffer) {
-        this.ingredient.toNetwork(buffer);
-        buffer.writeShort(this.amount);
-    }
 }
