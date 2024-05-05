9c4cc0fe5e4ac3fd36972e55a7d36108076c44f6
stal111 <stal111@gmx.de>
2024-05-05 23:26:21 +0200

    Delete old RenderUtils class

--- src/main/java/com/stal111/forbidden_arcanus/client/renderer/block/UtremJarRenderer.java
+++ src/main/java/com/stal111/forbidden_arcanus/client/renderer/block/UtremJarRenderer.java
@@ -2,15 +2,9 @@ package com.stal111.forbidden_arcanus.client.renderer.block;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.stal111.forbidden_arcanus.common.block.entity.UtremJarBlockEntity;
-import com.stal111.forbidden_arcanus.util.RenderUtils;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
-import net.minecraft.world.phys.AABB;
-import net.neoforged.neoforge.client.extensions.common.IClientFluidTypeExtensions;
-import net.neoforged.neoforge.fluids.FluidStack;
-import org.joml.Matrix3f;
-import org.joml.Matrix4f;
 
 import javax.annotation.Nonnull;
 
@@ -27,14 +21,5 @@ public class UtremJarRenderer implements BlockEntityRenderer<UtremJarBlockEntity
 
     @Override
     public void render(@Nonnull UtremJarBlockEntity blockEntity, float partialTicks, @Nonnull PoseStack poseStack, @Nonnull MultiBufferSource bufferSource, int packedLight, int packedOverlay) {
-        FluidStack fluidStack = blockEntity.getTank().getFluid();
-        int capacity = blockEntity.getTank().getCapacity();
-
-        if (!fluidStack.isEmpty() && capacity > 0) {
-            Matrix4f matrix = poseStack.last().pose();
-            Matrix3f normal = poseStack.last().normal();
-
-            RenderUtils.renderFluid(poseStack, blockEntity.getTank(), fluidStack, bufferSource, matrix, new AABB(3.5 / 16.0F, 0.5 / 16.0F, 3.5 / 16.0F, 12.5 / 16.0F, 12.5 / 16.0F, 12.5 / 16.0F), IClientFluidTypeExtensions.of(fluidStack.getFluid()).getTintColor(fluidStack.getFluid().defaultFluidState(), blockEntity.getLevel(), blockEntity.getBlockPos()), packedLight, packedOverlay);
-        }
     }
 }
