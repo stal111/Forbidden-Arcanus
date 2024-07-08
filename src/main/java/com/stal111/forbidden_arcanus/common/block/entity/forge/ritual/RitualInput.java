43a26048e6060aa9a7a34c44dae346e7b0d02991
stal111 <stal111@gmx.de>
2024-07-08 23:09:17 +0200

    Change RitualInput amount to require a positive int

--- src/main/java/com/stal111/forbidden_arcanus/common/block/entity/forge/ritual/RitualInput.java
+++ src/main/java/com/stal111/forbidden_arcanus/common/block/entity/forge/ritual/RitualInput.java
@@ -2,6 +2,7 @@ package com.stal111.forbidden_arcanus.common.block.entity.forge.ritual;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.ExtraCodecs;
 import net.minecraft.world.item.crafting.Ingredient;
 
 import java.util.Optional;
@@ -16,7 +17,7 @@ public record RitualInput(Ingredient ingredient, int amount) {
             Ingredient.CODEC_NONEMPTY.fieldOf("ingredient").forGetter(input -> {
                 return input.ingredient;
             }),
-            Codec.INT.optionalFieldOf("amount").forGetter(input -> {
+            ExtraCodecs.POSITIVE_INT.optionalFieldOf("amount").forGetter(input -> {
                 return Optional.of(input.amount);
             })
     ).apply(instance, (name, amount) -> {
