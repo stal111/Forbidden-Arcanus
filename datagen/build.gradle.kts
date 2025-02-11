bc12087a4e5dc59f18ff2fec55c59ed59f253edc
stal111 <stal111@gmx.de>
2025-02-11 18:55:49 +0100

    Bump ModDevGradle

--- datagen/build.gradle.kts
+++ datagen/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("eclipse")
     id("idea")
     id("maven-publish")
-    id("net.neoforged.moddev") version "2.0.42-beta"
+    id("net.neoforged.moddev") version "2.0.78"
 }
 
 val mod_id: String by project
