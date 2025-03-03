d2370966a4dee37d359d9aca517ae67db222d1ee
stal111 <stal111@gmx.de>
2025-03-03 18:39:40 +0100

    Remove player instabuild check from ritual start logic

--- neoforge/src/main/java/com/stal111/forbidden_arcanus/common/block/HephaestusForgeBlock.java
+++ neoforge/src/main/java/com/stal111/forbidden_arcanus/common/block/HephaestusForgeBlock.java
@@ -157,7 +157,7 @@ public class HephaestusForgeBlock extends Block implements SimpleWaterloggedBloc
         if (state.getValue(ACTIVATED) && ritualStarter != null && level.getBlockEntity(pos) instanceof HephaestusForgeBlockEntity blockEntity) {
             level.playSound(player, pos, ritualStarter.soundEvent().value(), SoundSource.PLAYERS, 0.85F, level.getRandom().nextFloat() * 0.15F + 0.9F);
 
-            if (!level.isClientSide() && blockEntity.getRitualManager().startRitual(blockEntity.getEssenceManager().getStorage()) && !player.getAbilities().instabuild) {
+            if (!level.isClientSide() && blockEntity.getRitualManager().startRitual(blockEntity.getEssenceManager().getStorage())) {
                 stack.hurtAndBreak(ritualStarter.damagePerRitual(), player, LivingEntity.getSlotForHand(hand));
             }
 
