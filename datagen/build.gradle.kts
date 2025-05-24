80d0fa2ae366389fc09cd750ce7c7593c2cbd5cf
stal111 <stal111@gmx.de>
2025-05-24 11:14:19 +0200

    Bump moddev gradle and update dependency versions

--- datagen/build.gradle.kts
+++ datagen/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("eclipse")
     id("idea")
     id("maven-publish")
-    id("net.neoforged.moddev") version "2.0.78"
+    id("net.neoforged.moddev") version "2.0.88"
 }
 
 val mod_id: String by project
