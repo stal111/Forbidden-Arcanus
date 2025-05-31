ec37f167cf2a1fa9292dc66586180b5b28d6ca99
stal111 <stal111@gmx.de>
2025-05-31 11:59:21 +0200

    Add wand use animation

--- neoforge/src/main/java/com/stal111/forbidden_arcanus/common/item/MagicWandItem.java
+++ neoforge/src/main/java/com/stal111/forbidden_arcanus/common/item/MagicWandItem.java
@@ -2,9 +2,14 @@ package com.stal111.forbidden_arcanus.common.item;
 
 import com.stal111.forbidden_arcanus.core.init.ModDataComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 
 import java.util.List;
 
@@ -17,5 +22,17 @@ public class MagicWandItem extends Item {
     @Override
     public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> components, TooltipFlag flag) {
         stack.addToTooltip(ModDataComponents.WAND_PARTS, context, components::add, flag);
+    }
+
+    @Override
+    public int getUseDuration(ItemStack stack, LivingEntity entity) {
+        return 200;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+        player.startUsingItem(usedHand);
+
+        return InteractionResultHolder.consume(player.getItemInHand(usedHand));
     }
 }
