5e09936fe0b2c0cb641773c7f707eeaaa0dc7f85
stal111 <stal111@gmx.de>
2025-06-14 12:28:46 +0200

    Improve aureal missile

--- neoforge/src/main/java/com/stal111/forbidden_arcanus/common/entity/projectile/AurealMissile.java
+++ neoforge/src/main/java/com/stal111/forbidden_arcanus/common/entity/projectile/AurealMissile.java
@@ -2,75 +2,103 @@ package com.stal111.forbidden_arcanus.common.entity.projectile;
 
 import com.stal111.forbidden_arcanus.core.init.ModEntities;
 import com.stal111.forbidden_arcanus.core.init.ModParticles;
-import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
+import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.ProjectileUtil;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.event.EventHooks;
 import org.jetbrains.annotations.NotNull;
 
-import javax.annotation.Nullable;
+public class AurealMissile extends Projectile {
 
-public class AurealMissile extends AbstractHurtingProjectile {
+    public double accelerationPower = 0.01D;
 
     public AurealMissile(EntityType<? extends AurealMissile> entityType, Level level) {
         super(entityType, level);
     }
 
     public AurealMissile(LivingEntity livingEntity, Level level, double x, double y, double z) {
-        super(ModEntities.AUREAL_MISSILE.get(), x, y, z, level);
+        super(ModEntities.AUREAL_MISSILE.get(), level);
         this.setOwner(livingEntity);
-        this.accelerationPower = 0.01D;
+        this.setPos(x, y, z);
+    }
+
+    @Override
+    protected void defineSynchedData(SynchedEntityData.@NotNull Builder builder) {
+
     }
 
     @Override
     public void tick() {
-        this.accelerationPower = 0.01D;
+        if (!this.level().hasChunkAt(this.blockPosition())) {
+            this.discard();
+            return;
+        }
+        super.tick();
 
-        Entity entity = this.getOwner();
-        if (this.level().isClientSide() || (entity == null || !entity.isRemoved()) && this.level().hasChunkAt(this.blockPosition())) {
-            super.tick();
-            if (this.shouldBurn()) {
-                this.igniteForSeconds(1.0F);
-            }
+        HitResult hitresult = ProjectileUtil.getHitResultOnMoveVector(this, this::canHitEntity, ClipContext.Block.COLLIDER);
+        if (hitresult.getType() != HitResult.Type.MISS && !EventHooks.onProjectileImpact(this, hitresult)) {
+            this.hitTargetOrDeflectSelf(hitresult);
+        }
 
-            HitResult hitresult = ProjectileUtil.getHitResultOnMoveVector(this, this::canHitEntity, this.getClipType());
-            if (hitresult.getType() != HitResult.Type.MISS && !EventHooks.onProjectileImpact(this, hitresult)) {
-                this.hitTargetOrDeflectSelf(hitresult);
-            }
-
-            this.checkInsideBlocks();
-            Vec3 vec3 = this.getDeltaMovement();
-            double d0 = this.getX() + vec3.x;
-            double d1 = this.getY() + vec3.y;
-            double d2 = this.getZ() + vec3.z;
-            ProjectileUtil.rotateTowardsMovement(this, 0.2F);
-            float f;
-            if (!this.isInWater()) {
-                f = this.getInertia();
-            } else {
-                for(int i = 0; i < 4; ++i) {
-                    float f1 = 0.25F;
-                    this.level().addParticle(ParticleTypes.BUBBLE, d0 - vec3.x * (double)0.25F, d1 - vec3.y * (double)0.25F, d2 - vec3.z * (double)0.25F, vec3.x, vec3.y, vec3.z);
-                }
-
-                f = this.getLiquidInertia();
-            }
-
-            this.setDeltaMovement(vec3.add(vec3.normalize().scale(this.accelerationPower)).scale(f));
-            if (this.tickCount % 4 == 0) {
-                this.level().addParticle(ModParticles.MAGIC_GLINT.get(), this.getX(), this.getY() + 0.2, this.getZ(), 0.0F, 0.0F, 0.0F);
-            }
-
-            this.setPos(d0, d1, d2);
+        this.checkInsideBlocks();
+        Vec3 vec3 = this.getDeltaMovement();
+        double d0 = this.getX() + vec3.x;
+        double d1 = this.getY() + vec3.y;
+        double d2 = this.getZ() + vec3.z;
+        ProjectileUtil.rotateTowardsMovement(this, 0.2F);
+        float f;
+        if (!this.isInWater()) {
+            f = this.getInertia();
         } else {
+            for (int i = 0; i < 4; ++i) {
+                this.level().addParticle(ParticleTypes.BUBBLE, d0 - vec3.x * (double) 0.25F, d1 - vec3.y * (double) 0.25F, d2 - vec3.z * (double) 0.25F, vec3.x, vec3.y, vec3.z);
+            }
+
+            f = this.getLiquidInertia();
+        }
+
+        this.setDeltaMovement(vec3.add(vec3.normalize().scale(this.accelerationPower)).scale(f));
+        if (this.tickCount % 4 == 0) {
+            this.level().addParticle(ModParticles.MAGIC_GLINT.get(), this.getX(), this.getY() + 0.2, this.getZ(), 0.0F, 0.0F, 0.0F);
+        }
+
+        this.setPos(d0, d1, d2);
+    }
+
+    @Override
+    protected void onHitEntity(@NotNull EntityHitResult result) {
+        super.onHitEntity(result);
+        if (this.level() instanceof ServerLevel serverLevel) {
+            Entity entity = result.getEntity();
+
+            if (this.getOwner() instanceof LivingEntity owner) {
+                owner.setLastHurtMob(entity);
+            }
+
+            DamageSource damagesource = this.damageSources().magic();
+
+            if (entity.hurt(damagesource, 5.0F) && entity instanceof LivingEntity livingEntity) {
+                EnchantmentHelper.doPostAttackEffects(serverLevel, livingEntity, damagesource);
+            }
+        }
+    }
+
+    @Override
+    protected void onHit(@NotNull HitResult result) {
+        super.onHit(result);
+        if (!this.level().isClientSide()) {
             this.discard();
         }
     }
@@ -81,13 +109,15 @@ public class AurealMissile extends AbstractHurtingProjectile {
     }
 
     @Override
-    protected boolean shouldBurn() {
-        return false;
+    public float getLightLevelDependentMagicValue() {
+        return 1.0F;
     }
 
-    @Nullable
-    @Override
-    protected ParticleOptions getTrailParticle() {
-        return null;
+    private float getInertia() {
+        return 0.95F;
+    }
+
+    private float getLiquidInertia() {
+        return 0.8F;
     }
 }
--- neoforge/src/main/java/com/stal111/forbidden_arcanus/common/item/MagicWandItem.java
+++ neoforge/src/main/java/com/stal111/forbidden_arcanus/common/item/MagicWandItem.java
@@ -40,8 +40,8 @@ public class MagicWandItem extends Item {
     @Override
     public void releaseUsing(ItemStack stack, Level level, LivingEntity livingEntity, int timeLeft) {
         if (!level.isClientSide() && (stack.getUseDuration(livingEntity) - timeLeft) >= 60) {
-            AurealMissile aurealMissile = new AurealMissile(livingEntity, level, livingEntity.position().x, livingEntity.getEyePosition().y, livingEntity.position().z);
-            aurealMissile.shootFromRotation(livingEntity, livingEntity.getXRot(), livingEntity.getYRot(), 0.0F, 0.8F, 0.0F);
+            AurealMissile aurealMissile = new AurealMissile(livingEntity, level, livingEntity.position().x(), livingEntity.getEyePosition().y(), livingEntity.position().z());
+            aurealMissile.shootFromRotation(livingEntity, livingEntity.getXRot(), livingEntity.getYRot(), 0.0F, 0.8F, 0.5F);
 
             level.addFreshEntity(aurealMissile);
         }
