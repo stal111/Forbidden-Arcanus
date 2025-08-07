02f17e197a356380946ac22025c58a4a85485aaa
stal111 <stal111@gmx.de>
2025-08-07 21:20:23 +0200

    Comment out tooltip display logic in WandParts

--- neoforge/src/main/java/com/stal111/forbidden_arcanus/common/item/component/WandParts.java
+++ neoforge/src/main/java/com/stal111/forbidden_arcanus/common/item/component/WandParts.java
@@ -4,7 +4,6 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.stal111.forbidden_arcanus.common.item.wand.WandPart;
 import com.stal111.forbidden_arcanus.common.item.wand.WandStats;
-import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.TooltipFlag;
@@ -34,14 +33,14 @@ public record WandParts(
 
     @Override
     public void addToTooltip(Item.TooltipContext context, Consumer<Component> tooltipAdder, TooltipFlag tooltipFlag) {
-        WandStats combinedStats = this.getCombinedStats();
-
-        tooltipAdder.accept(Component.literal("Tip: ").append(this.tip.materialName()).withStyle(ChatFormatting.GRAY));
-        tooltipAdder.accept(Component.literal("Transition: ").append(this.transition.materialName()).withStyle(ChatFormatting.GRAY));
-        tooltipAdder.accept(Component.literal("Pommel: ").append(this.pommel.materialName()).withStyle(ChatFormatting.GRAY));
-        tooltipAdder.accept(Component.empty());
-        tooltipAdder.accept(Component.literal("Damage: ").append(String.valueOf(combinedStats.damageBonus())).withStyle(ChatFormatting.GREEN));
-        tooltipAdder.accept(Component.literal("Speed: ").append(String.valueOf(combinedStats.speedBonus())).withStyle(ChatFormatting.GREEN));
-        tooltipAdder.accept(Component.literal("Aim: ").append(String.valueOf(combinedStats.aimBonus())).withStyle(ChatFormatting.GREEN));
+//        WandStats combinedStats = this.getCombinedStats();
+//
+//        tooltipAdder.accept(Component.literal("Tip: ").append(this.tip.materialName()).withStyle(ChatFormatting.GRAY));
+//        tooltipAdder.accept(Component.literal("Transition: ").append(this.transition.materialName()).withStyle(ChatFormatting.GRAY));
+//        tooltipAdder.accept(Component.literal("Pommel: ").append(this.pommel.materialName()).withStyle(ChatFormatting.GRAY));
+//        tooltipAdder.accept(Component.empty());
+//        tooltipAdder.accept(Component.literal("Damage: ").append(String.valueOf(combinedStats.damageBonus())).withStyle(ChatFormatting.GREEN));
+//        tooltipAdder.accept(Component.literal("Speed: ").append(String.valueOf(combinedStats.speedBonus())).withStyle(ChatFormatting.GREEN));
+//        tooltipAdder.accept(Component.literal("Aim: ").append(String.valueOf(combinedStats.aimBonus())).withStyle(ChatFormatting.GREEN));
     }
 }
