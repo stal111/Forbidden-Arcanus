96b7689da7b85d3d00ccdcf85e3e3006d88d9c65
stal111 <stal111@gmx.de>
2025-08-09 12:23:53 +0200

    Register new magic protection attribute

--- neoforge/src/main/java/com/stal111/forbidden_arcanus/common/entity/attribute/FAAttributes.java
+++ neoforge/src/main/java/com/stal111/forbidden_arcanus/common/entity/attribute/FAAttributes.java
@@ -5,6 +5,7 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
+import net.neoforged.neoforge.common.PercentageAttribute;
 import net.valhelsia.valhelsia_core.api.common.registry.helper.MappedRegistryHelper;
 
 public class FAAttributes {
@@ -12,5 +13,6 @@ public class FAAttributes {
     public static final MappedRegistryHelper<Attribute> HELPER = ForbiddenArcanus.REGISTRY_MANAGER.getHelper(Registries.ATTRIBUTE);
 
     public static final Holder<Attribute> AUREAL_REGENERATION = HELPER.register("generic.aureal_regeneration", () -> new RangedAttribute("attribute.name.generic.aureal_regeneration", 0.0D, 0.0D, Double.MAX_VALUE).setSyncable(true));
+    public static final Holder<Attribute> MAGIC_PROTECTION = HELPER.register("generic.magic_protection", () -> new PercentageAttribute("attribute.name.generic.magic_protection", 0.0D, 0.0D, 1.0).setSyncable(true));
 
 }
