0728d5f8d30e19ddc288866dc74b2b33b97ea846
stal111 <stal111@gmx.de>
2025-09-29 13:15:29 +0200

    Bump Gradle to 9.1

--- datagen/build.gradle.kts
+++ datagen/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("eclipse")
     id("idea")
     id("maven-publish")
-    id("net.neoforged.moddev") version "2.0.88"
+    id("net.neoforged.moddev") version "2.0.107"
 }
 
 val mod_id: String by project
