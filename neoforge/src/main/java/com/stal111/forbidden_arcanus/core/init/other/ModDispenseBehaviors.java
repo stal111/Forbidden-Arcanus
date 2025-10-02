95ee1530aaa463e3b7ec28b52c73061c8a1a2c93
linas-meinecke-vw <linas.meinecke@volkswagen.de>
2025-10-02 14:40:12 +0200

    Fix obsidian skulls missing equippable component

--- neoforge/src/main/java/com/stal111/forbidden_arcanus/core/init/other/ModDispenseBehaviors.java
+++ neoforge/src/main/java/com/stal111/forbidden_arcanus/core/init/other/ModDispenseBehaviors.java
@@ -5,25 +5,10 @@ import com.stal111.forbidden_arcanus.common.block.dispenser.SoulDispenseBehavior
 import com.stal111.forbidden_arcanus.core.init.ModItems;
 import net.minecraft.world.level.block.DispenserBlock;
 
-/**
- * Mod Dispense Behaviours <br>
- * Forbidden Arcanus - com.stal111.forbidden_arcanus.init.other.ModDispenseBehaviors
- *
- * @author stal111
- * @version 1.18.1 - 2.0.1
- * @since 2021-11-21
- */
 public class ModDispenseBehaviors {
 
     public static void registerDispenseBehaviors() {
         DispenserBlock.registerBehavior(ModItems.SOUL.get(), new SoulDispenseBehavior());
-        //TODO
-//        DispenserBlock.registerBehavior(ModBlocks.OBSIDIAN_SKULL.getSkull(), ObsidianSkullItem.DISPENSE_ITEM_BEHAVIOR);
-//        DispenserBlock.registerBehavior(ModBlocks.CRACKED_OBSIDIAN_SKULL.getSkull(), ObsidianSkullItem.DISPENSE_ITEM_BEHAVIOR);
-//        DispenserBlock.registerBehavior(ModBlocks.FRAGMENTED_OBSIDIAN_SKULL.getSkull(), ObsidianSkullItem.DISPENSE_ITEM_BEHAVIOR);
-//        DispenserBlock.registerBehavior(ModBlocks.FADING_OBSIDIAN_SKULL.getSkull(), ObsidianSkullItem.DISPENSE_ITEM_BEHAVIOR);
-//        DispenserBlock.registerBehavior(ModBlocks.AUREALIC_OBSIDIAN_SKULL.getSkull(), ObsidianSkullItem.DISPENSE_ITEM_BEHAVIOR);
-//        DispenserBlock.registerBehavior(ModBlocks.ETERNAL_OBSIDIAN_SKULL.getSkull(), ObsidianSkullItem.DISPENSE_ITEM_BEHAVIOR);
         DispenserBlock.registerProjectileBehavior(ModItems.BOOM_ARROW.get());
         DispenserBlock.registerProjectileBehavior(ModItems.DRACO_ARCANUS_ARROW.get());
         DispenserBlock.registerBehavior(ModItems.ARCANE_BONE_MEAL.get(), new ArcaneBoneMealDispenseBehavior());
